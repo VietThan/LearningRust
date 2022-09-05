896cbee9c0537b6d5b9daf7943315434d6a037d6
Viet Than <thanhoangviet@gmail.com>
2022-09-05 15:58:37 -0400

    got this working after updating rust

--- another_function/src/main.rs
+++ another_function/src/main.rs
@@ -1,5 +1,7 @@
 fn another_function(x: i32) {
-    println!("The value of x is: {x}", x=x);
+    println!("The value of x is: {x}");
+    // in rust 1.57 it was 
+    //println!("The value of x is: {x}", x=x);
 }
 
 fn main() {
